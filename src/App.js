5ce2d0f87979e4ff1346ee784a698253b483f5bb
pranam jain <pranamjain02@gmail.com>
2021-05-14 02:14:16 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import data from './data'
+import Article from './Article'
+
+const getStorageTheme = () => {
+  let theme = 'light-theme'
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme')
+  }
+  return theme
+}
+
+function App() {
+  const [theme, setTheme] = useState(getStorageTheme)
+
+  const changeColor = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme')
+    } else {
+      setTheme('light-theme')
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme
+    localStorage.setItem('theme', theme)
+  }, [theme])
+  return <main>
+    <nav>
+      <div className="nav-center">
+        <h1>Overrated</h1>
+        <button className="btn" onClick={changeColor}>toggle</button>
+      </div>
+    </nav>
+    <section className="articles">
+      {data.map((item) => {
+        return <Article key={item.id} {...item} />
+      })}
+    </section>
+  </main>
+}
+
+export default App
